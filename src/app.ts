75816b341e6ce33fbb0823bb6e88e2716a2475df
shohan-pherones <shohan.sub.56@gmail.com>
2024-05-16 16:31:31 +0600

    student api

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -9,5 +10,7 @@ app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use('/api/v1/students', StudentRoutes);
 
 export default app;
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export interface Guardian {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+}
+
+export interface UserName {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+}
+
+export interface LocalGuardian {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+}
+
+export interface Student {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'inactive';
+}
